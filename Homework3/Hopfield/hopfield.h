c627933c5972eab5cd4e3fd43e364c652ff732ce
rokrust <rokrust@hotmail.com>
2017-01-16 16:06:31 +0100

    barely working

--- Homework3/Hopfield/hopfield.h
+++ Homework3/Hopfield/hopfield.h
@@ -9,7 +9,7 @@
 struct Node{
 	double weight[HEIGHT][WIDTH];
 	int row, col;
-
+	
 }; typedef struct Node Node;
 
 
@@ -19,47 +19,35 @@ struct Hopfield_network{
 }; typedef struct Hopfield_network Hopfield_network;
 
 
-Hopfield_network* hopfield_init(int training_image[N_IMAGES_MAX][HEIGHT][WIDTH], int n_training_images){
-	Hopfield_network* network;
+void hopfield_print_int_image(int image[HEIGHT][WIDTH]);
 
-	for(int i = 0; i < HEIGHT; i++){
-		for(int j = 0; j < WIDTH; j++){
-			network->node[i][j] = node;
-		}
-	}
+
+Hopfield_network* hopfield_init(int training_image[N_IMAGES_MAX][HEIGHT][WIDTH], int n_training_images){
+	Hopfield_network* network = malloc(sizeof(Hopfield_network));
+
 	
 	//Iterate through all the images
 	for(int image = 0; image < n_training_images; image++){
-		printf("Image: %d\n", image);
 
 		//Iterate through all the nodes
 		for(int row = 0; row < HEIGHT; row++){
-			printf("Row: %d\n", row);
 
 			for(int col = 0; col < WIDTH; col++){
-				printf("Col: %d\n", col);
-
+			
 				network->node[row][col].row = row;
 				network->node[row][col].col = col;				
 				network->node[row][col].weight[row][col] = 0;
 				
 				//For initializing every weight in the current node
 				for(int i = 0; i < HEIGHT; i++){	
-					printf("i: %d\n", i);
-
-					for(int j = i + 1; j < WIDTH; j++){
-						printf("j :%d\n", j);
+					for(int j = 0; j < WIDTH; j++){
 						//initial weight is the product of the two 	
 						//pixel values
-						double initial_weight = 			
+						double initial_weight = 		
 							training_image[image][row][col] * 
 							training_image[image][i][j];
-
+						
 						network->node[row][col].weight[i][j] += 
-												initial_weight;
-							
-
-						network->node[row][col].weight[j][i] += 
 												initial_weight;
 
 					}
@@ -67,20 +55,22 @@ Hopfield_network* hopfield_init(int training_image[N_IMAGES_MAX][HEIGHT][WIDTH],
 			}
 		}
 	}
-	
+
 	return network;
 }
 
 int hopfield_read_training_data(FILE* fp, int training_image[N_IMAGES_MAX][HEIGHT][WIDTH]){
-	char inputString[WIDTH];
+	char inputString[WIDTH+2]; //Include newline and null
+	inputString[WIDTH+1] = '\0';
 
 	int image_index = 0;
-	int row = 1;
 
-	fgets(inputString, 200, fp);
+
 	while(inputString[1] != '-'){
-		for(row; row < HEIGHT + 1; row++){
-			//printf("row: %d, %s", row, inputString);
+
+		for(int row = 0; row < HEIGHT; row++){
+			fgets(inputString, 200, fp);
+
 			for(int col = 0; col < WIDTH; col++){
 				
 				if(inputString[col] == '*'){
@@ -91,61 +81,105 @@ int hopfield_read_training_data(FILE* fp, int training_image[N_IMAGES_MAX][HEIGH
 					training_image[image_index][row][col] = -1;
 				}
 			}
-			//printf("row: %d\n", row);
-
-			fgets(inputString, 200, fp);
 		}
-		row = 0;
-
+		fgets(inputString, 200, fp);
 		image_index++;
 	}
-
+	
 	return image_index - 1;
 }
 
 
 int hopfield_read_running_data(FILE* fp, int distorted_image[N_IMAGES_MAX][HEIGHT][WIDTH]){
-	char inputString[WIDTH];
+	char inputString[WIDTH+2];
+	inputString[WIDTH+1] = '\0';
 
 	int image_index = 0;
-	
-	while(fgets(inputString, WIDTH, fp) != 0){
+	int stay_in_loop = 1;
+
+	while(stay_in_loop){
+
 		for(int row = 0; row < HEIGHT; row++){
+			fgets(inputString, 200, fp);
+
 			for(int col = 0; col < WIDTH; col++){
 					
 				if(inputString[col] == '*'){
 					distorted_image[image_index][row][col] = 1;
 				}
-
+	
 				else if(inputString[col] == '.'){
 					distorted_image[image_index][row][col] = -1;
 				}
 
 			}
 			
-			fgets(inputString, WIDTH, fp);
 		}
-
+		
+		stay_in_loop = fgets(inputString, 200, fp);
 		image_index++;
-		fgets(inputString, WIDTH, fp);
-	
-	}	
+	}
 
 	return image_index;
-
+	
 }
 
-double hopfield_node_output(Hopfield_network* network, int row, int col){
+double hopfield_node_output(Node node, int image[HEIGHT][WIDTH]){
+	double output = 0;
+	
 	for(int i = 0; i < HEIGHT; i++){
 		for(int j = 0; j < WIDTH; j++){
-			double w_ij = network->node[row][col].weight[i][j];
+			double w_ij = node.weight[i][j];
 			
+		
+			output += w_ij*image[i][j];	
 			
 		}
 	}
 	
 }
 
+void hopfield_network_output(Hopfield_network* network, int image[HEIGHT][WIDTH], char output_image[HEIGHT][WIDTH]){
+
+	for(int row = 0; row < HEIGHT; row++){
+		for(int col = 0; col < WIDTH; col++){
+			double node_output = 
+				hopfield_node_output(network->node[row][col], image);
+			
+			if(node_output >= 0){
+				output_image[row][col] = '*';
+			}
+
+			else{
+				output_image[row][col] = '.';
+			}
+		}
+	}
+}
+
 void hopfield_update_weights(Hopfield_network* network){
 	
+}
+
+void hopfield_print_image(char image[HEIGHT][WIDTH]){
+	for(int row = 0; row < HEIGHT; row++){
+		for(int col = 0; col < WIDTH; col++){
+			printf("%c", image[row][col]);
+		}
+		printf("\n");
+	}
+}
+
+void hopfield_print_int_image(int image[HEIGHT][WIDTH]){
+	for(int row = 0; row < HEIGHT; row++){
+		for(int col = 0; col < WIDTH; col++){
+			if(image[row][col] == 1){
+				printf("*");
+			}
+			else if(image[row][col] == -1){
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
 }
--- Homework3/Hopfield/main.c
+++ Homework3/Hopfield/main.c
@@ -7,18 +7,28 @@ int main(){
 
 	int training_image[N_IMAGES_MAX][HEIGHT][WIDTH];
 	int distorted_image[N_IMAGES_MAX][HEIGHT][WIDTH];
-	
-	printf("Training data\n");
+	char output_image[HEIGHT][WIDTH];	
+
 	int n_training_images = 
 			hopfield_read_training_data(fp, training_image);
 	
-	printf("Running data\n");
 	int n_distorted_images = 
 			hopfield_read_running_data(fp, distorted_image);
 
-	printf("Init\n");
 	Hopfield_network *network = 
 			hopfield_init(training_image, n_training_images);
+
+		for(int i = 0; i < n_distorted_images-1; i++){
+			hopfield_network_output(network, distorted_image[i], 
+									output_image);
+			hopfield_print_image(output_image);
+			printf("-\n");
+		}
+
+		hopfield_network_output(network, 
+								distorted_image[n_distorted_images-1], 
+								output_image);
+		hopfield_print_image(output_image);
 
 	return 0;
 }
