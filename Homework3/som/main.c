366c9ae586720bfc43e52c3cf62811df98b3ead7
Student 03 Lab RoSo <lab_roso_stud03@kaon.int.rcs.ei.tum.de>
2017-01-20 20:37:10 +0100

    Somewhat working tsp

--- Homework3/som/main.c
+++ Homework3/som/main.c
@@ -6,24 +6,13 @@ int main(){
     Som_network* network = malloc(sizeof(Som_network));
     int data[1000];
     Position pos[1000];
+    Ellipse ellipse;
 
-    printf("Read data\n");
-    int n_cities = som_read_data(data, pos);
+    int n_cities = som_read_data(data, pos, &ellipse);
 
-    for(int i = 0; i < n_cities; i++){
-        printf("%d: %lf, %lf\n", i + 1, pos[i].x, pos[i].y);
-    }
+    som_init_network(network, ellipse);
 
-    printf("\nInit\n");
-    som_init_network(network);
-
-    //printf("Run network\n");
     som_run_network(network, pos, n_cities);
 
-    for(int i = 0; i < N_NODES; i++){
-        printf("%d: %lf, %lf\n", i + 1, network->node[i].pos.x, network->node[i].pos.y);
-    }
-
-    //printf("Print path\n");
-    //som_print_path(network);
+    som_print_path(network);
 }
--- Homework3/som/som.h
+++ Homework3/som/som.h
@@ -1,16 +1,19 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <stdlib.h>
 
-#define N_NODES 16
-#define START_RADIUS 1
-#define RUNTIME_US 5
+
+#define RUNTIME_S 50
 #define NEIGHBORHOOD_RADIUS_STEP 0.001
+#define AMOUNT_OF_NEIGHBORS_TO_MOVE 3
 #define M_PI 3.1415925
 
-double learning_rate = 0.1;
-double neighbor_var = 10;
-double neighborhood_radius = 0.1;
+int N_NODES;
+double learning_rate = 0.3;
+double neighbor_var = 500;
+double neighborhood_radius = 500;
+double termination_threshold;
 
 struct Position{
     double x;
@@ -18,29 +21,34 @@ struct Position{
 
 }; typedef struct Position Position;
 
+struct Ellipse{
+    Position center;
+    double a;
+    double b;
+
+}; typedef struct Ellipse Ellipse;
+
 
 //Might have to add a variable for affiliated city
 struct Node{
-	Position pos;
-    int closest_city;
+    Position pos;
+    int* closest_city;
+    int n_closest_cities;
 
 }; typedef struct Node Node;
 
 struct Som_network{
-	Node node[N_NODES];
+    Node* node;
 
 }; typedef struct Som_network Som_network;
 
 
 double som_distance(Position pos_a, Position pos_b);
 int som_determine_closest_node(Som_network* network, Position city_pos);
-void som_update_node_positions(Som_network* network, Position closest_node_pos, Position city_pos, int city_index);
+void som_update_node_positions(Som_network* network, int winning_node_index, Position city_pos);
 void som_scale_input(Position pos[1000], int n_elements, double scaling_value);
-
-void update_learning_rate(int iteration);
-void update_neighborhood_variance(int iteration);
-void update_neighborhood_radius();
-
+int som_terminate(Som_network* network, Position pos[1000], int n_cities);
+void update_tuning_parameters();
 
 
 
@@ -52,39 +60,73 @@ void update_neighborhood_radius();
 
 
 //Puts input into data and returns number of elements
-int som_read_data(int data[1000], Position pos[1000]){
+int som_read_data(int data[1000], Position pos[1000], Ellipse* ellipse){
 	FILE* fp = fopen("testInput23A.txt", "r");
     char input_string[100];
 
-    int largest_value = 0;
+    int largest_x = -10000000;
+    int largest_y = -10000000;
+    int smallest_x = 10000000;
+    int smallest_y = 10000000;
 	
 	int i = 0;
     while(fgets(input_string, 200, fp) != 0){
         sscanf(input_string, "%d,%lf,%lf\n", &(data[i]), &(pos[i].x), &(pos[i].y));
-		
-        if(pos[i].x > largest_value){
-            largest_value = pos[i].x;
+        int x = pos[i].x;
+        int y = pos[i].y;
+
+        //For calculating initial ellipse for nodes
+        if(x > largest_x){
+            largest_x = x;
         }
 
-        if(pos[i].y > largest_value){
-            largest_value = pos[i].y;
+        if(x < smallest_x){
+            smallest_x = x;
+        }
+
+        if(y > largest_y){
+            largest_y = y;
+        }
+
+        if(y < smallest_y){
+            smallest_y = y;
         }
 
 		i++;
 	}
 
-    som_scale_input(pos, i, 1.0/largest_value);
+    ellipse->a = (largest_x - smallest_x) / 2;
+    ellipse->b = (largest_y - smallest_y) / 2;
+
+    ellipse->center.x = (double)largest_x - ellipse->a;
+    ellipse->center.y = (double)largest_y - ellipse->b;
+
+    neighborhood_radius = (ellipse->a + ellipse->b) * 2;
+
+    neighbor_var = i/5;
+    termination_threshold = neighborhood_radius/15;
+
+
+    N_NODES = (int)(1.4 * i);
 
 	return i;	
 }
 
 //Should put all nodes evenly on a circle
-void som_init_network(Som_network* network){
-	for(int i = 0; i < N_NODES; i++){
-        network->node[i].pos.x = START_RADIUS*cos((2*M_PI / N_NODES) * i);
-        network->node[i].pos.y = START_RADIUS*sin((2*M_PI / N_NODES) * i);
+void som_init_network(Som_network* network, Ellipse ellipse){
+    network->node = malloc(sizeof(Node)*N_NODES);
 
+    for(int i = 0; i < N_NODES; i++){
+        network->node[i].closest_city = malloc(sizeof(int)*N_NODES);
+    }
+
+	for(int i = 0; i < N_NODES; i++){
+        network->node[i].pos.x = ellipse.center.x + ellipse.a * cos((2*M_PI / N_NODES) * i);
+        network->node[i].pos.y = ellipse.center.y + ellipse.b * sin((2*M_PI / N_NODES) * i);
+
+        network->node[i].n_closest_cities = 0;
 	}
+
 
 }
 
@@ -92,25 +134,38 @@ void som_run_network(Som_network* network, Position pos[1000], int n_cities){
     time_t start_time = time(NULL);
     time_t end_time = time(NULL);
 
-    for(long int i = 0; end_time - start_time < RUNTIME_US; i++){
+    int terminate = 0;
 
+    for(long int j = 0; end_time - start_time < RUNTIME_S && !terminate; j++){
         for(int city = 0; city < n_cities; city++){
+
             int closest_node = som_determine_closest_node(network, pos[city]);
-            som_update_node_positions(network, network->node[closest_node].pos, pos[city], city);
+            som_update_node_positions(network, closest_node, pos[city]);
         }
 
 
-        //update_learning_rate(i);
-        //update_neighborhood_variance(i);
-        //update_neighborhood_radius();
-
+        update_tuning_parameters();
+        terminate = som_terminate(network, pos, n_cities);
         end_time = time(NULL);
+    }
+
+    for(int i = 0; i < n_cities; i++){
+        int node_index = som_determine_closest_node(network, pos[i]);
+
+        int n_closest_cities = network->node[node_index].n_closest_cities++;
+
+        network->node[node_index].closest_city[n_closest_cities] = i;
     }
 }
 
 void som_print_path(Som_network* network){
     for(int i = 0; i < N_NODES; i++){
-        printf("%d\n", network->node[i].closest_city + 1);
+        int n_closest_cities = network->node[i].n_closest_cities;
+        Position cur_pos = network->node[i].pos;
+
+        for(int j = 0; j < n_closest_cities; j++){
+            printf("%d\n", network->node[i].closest_city[j] + 1);
+        }
     }
 }
 
@@ -120,6 +175,47 @@ void som_print_path(Som_network* network){
 //                         Private functions                               //
 //                                                                         //
 //*************************************************************************//
+int som_terminate(Som_network* network, Position pos[1000], int n_cities){
+    double longest_distance = 0;
+
+    for(int i = 0; i < n_cities; i++){
+        double city_shortest_distance = 1000000;
+        for(int j = 0; j < N_NODES; j++){
+            double cur_distance = som_distance(network->node[j].pos, pos[i]);
+            if(cur_distance < city_shortest_distance){
+                city_shortest_distance = cur_distance;
+            }
+
+        }
+
+        if(city_shortest_distance > longest_distance){
+            longest_distance = city_shortest_distance;
+
+        }
+    }
+    printf("threshold %lf\n", termination_threshold);
+    printf("longest distance %lf\n", longest_distance);
+    if(longest_distance > 0.5){
+        return 0;
+    }
+
+    else{
+        return 1;
+    }
+}
+
+
+int som_ring_buffer_index(int i){
+    if(i < 0){
+        return N_NODES + (i % N_NODES);
+    }
+
+    else{
+        return i % N_NODES;
+    }
+}
+
+
 void som_scale_input(Position pos[1000], int n_elements, double scaling_value){
     for(int i = 0; i < n_elements; i++){
         pos[i].x *= scaling_value;
@@ -137,7 +233,7 @@ double som_distance(Position pos_a, Position pos_b){
 
 //Returns the index of the node closest to city_pos
 int som_determine_closest_node(Som_network* network, Position city_pos){
-	int closest_node = 0;
+    int closest_node;
     double distance = 10000000;
 
 	for(int i = 0; i < N_NODES; i++){
@@ -153,47 +249,66 @@ int som_determine_closest_node(Som_network* network, Position city_pos){
 	return closest_node;
 }
 
-void som_update_node_positions(Som_network* network, Position closest_node_pos, Position city_pos, int city_index){
+void som_update_node_positions(Som_network* network, int winning_node_index, Position city_pos){
 
-    for(int i = 0; i < N_NODES; i++){
-        Position cur_pos = network->node[i].pos;
-        double d = som_distance(closest_node_pos, cur_pos);
-        //printf("d1: %lf\n", d);
+    Position closest_node_pos = network->node[winning_node_index].pos;
 
-        //Move all nodes that are within a certain radius of the winning node
+    //Iterate through neighbors of the winning node and check if they are close enough
+    //to qualify for moving
+    int start_value = winning_node_index - AMOUNT_OF_NEIGHBORS_TO_MOVE;
+    int end_value = winning_node_index + AMOUNT_OF_NEIGHBORS_TO_MOVE + 1;
+    for(int i = start_value; i < end_value; i++){
+        int node_index = som_ring_buffer_index(i);
+
+        Position current_node_pos = network->node[node_index].pos;
+
+        double d = som_distance(closest_node_pos, current_node_pos);
+
+
+        //Move all neighboring nodes that are within a certain radius of the winning node
         if(d < neighborhood_radius){
 
-            //printf("radius: %lf\n", neighborhood_radius);
-            if(N_NODES - d < d){
-                d = N_NODES - d;
-            }
+            double f = exp(-(d*d/(neighbor_var*neighbor_var))); //neighborhood function
 
-            printf("d2: %lf\n", d);
-            double f = exp(-d*d/(neighbor_var*neighbor_var)); //neighborhood function
-            //printf("f: %lf\n", f);
             Position new_pos;
-            new_pos.x = cur_pos.x + learning_rate * f * (city_pos.x - cur_pos.x);
-            new_pos.y = cur_pos.y + learning_rate * f * (city_pos.y - cur_pos.y);
+            new_pos.x = current_node_pos.x + learning_rate * f * (city_pos.x - current_node_pos.x);
+            new_pos.y = current_node_pos.y + learning_rate * f * (city_pos.y - current_node_pos.y);
 
+            network->node[node_index].pos = new_pos;
 
-            network->node[i].pos = new_pos;
-            network->node[i].closest_city = city_index;
         }
-
     }
+
+
+}
+
+void update_learning_rate(){
+    if(learning_rate < 0.01){
+        return;
+    }
+
+    learning_rate -= 0.001;
 }
 
 
-void update_learning_rate(int iteration){
-    learning_rate = 1.0/iteration;
-    printf("learning rate: %lf\n", learning_rate);
-}
+void update_neighborhood_variance(){
+    if(neighbor_var < 10){
+        return;
+    }
 
-
-void update_neighborhood_variance(int iteration){
-    neighbor_var = neighbor_var * (1 - 0.01 * iteration);
+    neighbor_var -= 0.01;
 }
 
 void update_neighborhood_radius(){
+    if(neighborhood_radius < 4){
+        return;
+    }
+
     neighborhood_radius -= NEIGHBORHOOD_RADIUS_STEP;
+}
+
+void update_tuning_parameters(){
+    update_learning_rate();
+    update_neighborhood_variance();
+    update_neighborhood_radius();
 }
